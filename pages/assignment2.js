c657c05b58d6f64a789fc7ea9866b5f6650fe357
Khai-Glitch <54896708+Khai-Glitch@users.noreply.github.com>
2019-10-09 08:36:23 -0400

    cahnge title

---
+++ pages/assignment2.js
@@ -0,0 +1,9 @@
+function angle calculator(){
+var angle_1;
+var angle_2; 
+var angle_3;
+var angle_1=prompt("enter angle 1");
+var angle_2=prompt("enter angle 2");
+var angle_3=180 - (angle_1 + angle_2);
+alert(angle_3)
+}
