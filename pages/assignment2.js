efe236b50e4492985af35fb5f8b95309817fc769
Khai-Glitch <54896708+Khai-Glitch@users.noreply.github.com>
2019-10-09 09:13:08 -0400

    correction

--- pages/assignment2.js
+++ pages/assignment2.js
@@ -1,9 +1,10 @@
-function angle calculator(){
-var angle_1;
-var angle_2; 
-var angle_3;
-var angle_1=prompt("enter angle 1");
-var angle_2=prompt("enter angle 2");
-var angle_3=180 - (angle_1 + angle_2);
-alert(angle_3)
+function angle_calculator(){
+	var angle_1
+	var angle_2
+	var angle_3
+	var angle_1=parseInt(prompt("enter angle 1"));
+	var angle_2=parseInt(prompt("enter angle 2"));
+	var angle_3=180 - (angle_1 + angle_2);
+	alert(angle_3);
 }
+
