a31b70f1d808c3ad665598b313e297fbca23b92c
Khai-Glitch <54896708+Khai-Glitch@users.noreply.github.com>
2019-10-16 09:58:36 -0400

    century counter

--- pages/assignment2.js
+++ pages/assignment2.js
@@ -25,3 +25,11 @@ function forloop() {
 	alert("Mine too!");
 }
 
+function centuryFromYear(year) {
+ 	 let centuryCount = 0;
+  	while (year > 0){
+    	year = year - 100;
+    	centuryCount = centuryCount + 1;
+ 	 }
+  	return centuryCount;
+	}
