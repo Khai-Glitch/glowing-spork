7ab7305b815749adce5427403769e072f3772782
Khai-Glitch <54896708+Khai-Glitch@users.noreply.github.com>
2019-10-29 09:23:40 -0400

    fix

--- pages/assignment2.js
+++ pages/assignment2.js
@@ -1,12 +1,12 @@
-function angle_calculator(){
-	var angle_1
-	var angle_2
-	var angle_3
-	var angle_1=parseInt(prompt("enter angle 1"));
-	var angle_2=parseInt(prompt("enter angle 2"));
-	var angle_3=180 - (angle_1 + angle_2);
-	alert(angle_3);
-}
+// function angle_calculator(){
+// 	var angle_1
+// 	var angle_2
+// 	var angle_3
+// 	var angle_1=parseInt(prompt("enter angle 1"));
+// 	var angle_2=parseInt(prompt("enter angle 2"));
+// 	var angle_3=180 - (angle_1 + angle_2);
+// 	alert(angle_3);
+// }
 
 // function centuryFromYear(){
 // 	let year = 1992;
@@ -18,22 +18,22 @@ function angle_calculator(){
 // 	}
 // }
 
-// function tenFour(){
-// 	 for ( var i = 1; i <= 100; i++ ){
-// 		 if ( i%4 === 0 && i%10 === 0 ){
-// 		 	alert('k');
-// 		 }
+function tenFour(){
+	 for ( var i = 1; i <= 100; i++ ){
+		 if ( i%4 === 0 && i%10 === 0 ){
+		 	alert('k');
+		 }
 		 
-// 		console.log("TenFour"); 
-// 		else if ( i%4 === 0 ){
-// 			console.log("Four");
-// 		}
-// 		else if ( i%10 === 0 ){
-// 			console.log("Ten");
-// 		}
-// 		else { 
-// 			console.log(i);
-// 		}
-// 	}
-// }
+		console.log("TenFour"); 
+		else if ( i%4 === 0 ){
+			console.log("Four");
+		}
+		else if ( i%10 === 0 ){
+			console.log("Ten");
+		}
+		else { 
+			console.log(i);
+		}
+	}
+}
 
