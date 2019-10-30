ef3b6e2ee947707619b399c8bd3d128537f3d915
Khai-Glitch <54896708+Khai-Glitch@users.noreply.github.com>
2019-10-30 10:42:22 -0400

    better brightside

--- pages/assignment2.js
+++ pages/assignment2.js
@@ -3,7 +3,13 @@ function angle_calculator(){
 	var angle_2
 	var angle_3
 	var angle_1=parseInt(prompt("enter angle 1"));
+	// if (angle_1 == NaN) {
+	// 		alert("That's not a number!");
+	// 	}
 	var angle_2=parseInt(prompt("enter angle 2"));
+	// if (angle_2 == NaN) {
+	// 		alert("That's not a number!");
+	// 	}
 	var angle_3=180 - (angle_1 + angle_2);
 	alert(angle_3);
 }
