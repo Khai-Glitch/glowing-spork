7e5eee32174fb68e367c451d5ade1376a9a5ebdc
Khai-Glitch <54896708+Khai-Glitch@users.noreply.github.com>
2019-12-16 09:53:06 -0500

    encoding program

---
+++ pages/encode.js
@@ -0,0 +1,108 @@
+function encodev1(){
+let str = prompt("enter phrase");
+str=str.replace("a","and");
+str=str.replace("b","when");
+str=str.replace("c","well");
+str=str.replace("d","six");
+str=str.replace("e","left");
+str=str.replace("f","very");
+str=str.replace("g","hope");
+str=str.replace("h","blue");
+str=str.replace("i","is");
+str=str.replace("j","swell");
+str=str.replace("k","at");
+str=str.replace("l","aa");
+str=str.replace("m","us");
+str=str.replace("n","may");
+str=str.replace("o","xi");
+str=str.replace("p","yes");
+str=str.replace("q","yea");
+str=str.replace("r","za");
+str=str.replace("s","so");
+str = str.replace("t","up");
+str=str.replace("u","uh");
+str=str.replace("v","how");
+str=str.replace("w","si");
+str=str.replace("x","ow");
+str=str.replace("y","ka");
+str=str.replace("z","ki");
+str=str.replace(" ",",");
+str=str.replace(",","?");
+str=str.replace("?","%");
+str=str.replace("!",";");
+console.log(str)
+
+}
+
+// function encodev1(){
+// 			let	str = prompt("enter phrase");
+// 				str.replace("a","and");
+// 				str.replace("b","when");
+// 				str.replace("c","well");
+// 				str.replace("d","six");
+// 				str.replace("e","left");
+// 				str.replace("f","very");
+// 				str.replace("g","hope");
+// 				str.replace("h","blue");
+// 				str.replace("i","is");
+// 				str.replace("j","swell");
+// 				str.replace("k","at");
+// 				str.replace("l","aa");
+// 				str.replace("m","us");
+// 				str.replace("n","may");
+// 				str.replace("o","xi");
+// 				str.replace("p","yes");
+// 				str.replace("q","yea");
+// 				str.replace("r","za");
+// 				str.replace("s","so");
+// 				str = str.replace("t","up");
+// 				str.replace("u","uh");
+// 				str.replace("v","how");
+// 				str.replace("w","si");
+// 				str.replace("x","ow");
+// 				str.replace("y","ka");
+// 				str.replace("z","ki");
+// 				str.replace(" ",",");
+// 				str.replace(",","?");
+// 				str.replace("?","%");
+// 				str.replace("!",";");
+// console.log(str)
+
+// }
+
+// function encodev2(){
+// 	let plaintext = 
+// 	 plaintext =parceInt(prompt("enter phrase"));
+// 		str.replace("a","β");
+// 		str.replace("b","γ");
+// 		str.replace("c","δ");
+// 		str.replace("d","ε");
+// 		str.replace("e","ζ");
+// 		str.replace("f","η");
+// 		str.replace("g","θ");
+// 		str.replace("h","ι");
+// 		str.replace("i","κ");
+// 		str.replace("j","λ");
+// 		str.replace("k","μ");
+// 		str.replace("l","ν");
+// 		str.replace("m","ξ");
+// 		str.replace("n","ο");
+// 		str.replace("o","π");
+// 		str.replace("p","ρ");
+// 		str.replace("q","σ");
+// 		str.replace("r","τ");
+// 		str.replace("s","υ");
+// 		str.replace("t","φ");
+// 		str.replace("u","χ");
+// 		str.replace("v","ψ");
+// 		str.replace("w","ω");
+// 		str.replace("x","α");
+// 		str.replace("y","@");
+// 		str.replace("z","*");
+// 		str.replace(" ",",");
+// 		str.replace(",","?");
+// 		str.replace("?","%");
+// 		str.replace("!",";");
+	
+// 	console.log(plaintext)
+// }
